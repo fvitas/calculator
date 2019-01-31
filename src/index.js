9ca24f496d10d9f774e4bcf21d24b193bfce4432
Filip Vitas <filip.vitas.dev@gmail.com>
2019-01-31 22:42:25 +0100

    move service worker registration to index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import { h, render } from 'preact'
 import App from './components/app'
 
 render(<App />, document.querySelector('#app'))
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load',
+        () => {
+            navigator.serviceWorker.register('/sw.js')
+                     .then(() => console.log('Service Worker registration successful'))
+        },
+        { once: true }
+    )
+}
